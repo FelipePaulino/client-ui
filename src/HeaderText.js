b0a420086f192f1339977a088e27980527fbfec3
Felipe Paulino <felipepaulino08@hotmail.com>
2023-08-11 09:59:43 -0300

    first commit

---
+++ src/HeaderText.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+function HeaderText(){
+    return <div>Teste de lib</div>
+}
+
+export default HeaderText
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from 'react-dom'
+import HeaderText from "./HeaderText";
+
+ReactDOM.render(<HeaderText/>, document.querySelector("#app"));
